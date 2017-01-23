cd11cc86f8fc1dbcab8c9fbf5edf29fb879dd654
baltazarparra <baltazarparra@outlook.com>
2017-01-23 05:35:36 -0200

    otimizando js

--- main.js
+++ main.js
@@ -2,8 +2,6 @@
 
     'use strict';
 
-    var $main = document.querySelector('[data-js="main"]');
-    var $main = document.querySelector('[data-js="main"]');
     var $handsYou = document.querySelector('[data-js="handsYou"]');
     var $handsCpu = document.querySelector('[data-js="handsCpu"]');
     var $scoreYou = document.querySelector('[data-js="scoreYou"]');
@@ -37,36 +35,26 @@
                 if (userChoice === 2 && result % 2 == 0) {
                     $statusTitle.textContent = 'Ganhou';
                     userScore += 1;
-                    app.winHandle(cpuChoice, userChoice, result);
+
                 } else if (userChoice === 2 && result % 2 != 0) {
                     $statusTitle.textContent = 'Perdeu';
                     cpuScore += 1;
-                    app.looseHandle(cpuChoice, userChoice, result);
+
                 } else if (userChoice === 1 && result % 2 != 0) {
                     $statusTitle.textContent = 'Ganhou';
                     userScore += 1;
-                    app.winHandle(cpuChoice, userChoice, result);
+
                 } else {
                     $statusTitle.textContent = 'Perdeu';
                     cpuScore += 1;
-                    app.looseHandle(cpuChoice, userChoice, result);
                 }
+
+                app.handleScore(cpuChoice, userChoice, result);
                 $scoreYou.textContent = userScore;
                 $scoreCpu.textContent = cpuScore;
             },
 
-            winHandle: function winHandle(cpuChoice, userChoice, result) {
-                $handsYou.textContent = '';
-                $handsCpu.textContent = '';
-                var $yourChoiceImg = document.createElement('img');
-                var $cpuChoiceImg = document.createElement('img');
-                $yourChoiceImg.setAttribute('src', 'svgs/' + userChoice + '.svg');
-                $cpuChoiceImg.setAttribute('src', 'svgs/' + cpuChoice + '.svg');
-                $handsYou.appendChild($yourChoiceImg);
-                $handsCpu.appendChild($cpuChoiceImg);
-            },
-
-            looseHandle: function looseHandle(cpuChoice, userChoice, result) {
+            handleScore: function handleScore(cpuChoice, userChoice, result) {
                 $handsYou.textContent = '';
                 $handsCpu.textContent = '';
                 var $yourChoiceImg = document.createElement('img');
